495ee61b40e2dcd3371466a44f4aab4ecd999b3b
Zevier <87379749+Zevier02@users.noreply.github.com>
2024-10-15 20:13:19 +0200

    Add files via upload

--- scripts/bgcolor.js
+++ scripts/bgcolor.js
@@ -1,11 +1,22 @@
 var checkBox = document.getElementById("color");
+var colors = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)]
 document.body.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+document.getElementById('shopt').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
+document.getElementById('upgrades').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
+document.getElementById('buildings').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
 function a(){
     if(checkBox.checked){
+        colors = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)]
         document.body.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        document.getElementById('shopt').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
+        document.getElementById('upgrades').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
+        document.getElementById('buildings').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
     }
     else {
-        document.body.style.background = "white"
+        document.body.style.background = "lightblue"
+        document.getElementById('shopt').style = `border-color: blue; background: url("images/bgshop.png");`
+        document.getElementById('upgrades').style = `border-color: gold;`
+        document.getElementById('buildings').style = `border-color: grey;`
     }
 }
 setInterval(a, 500)
--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,7 +1,52 @@
-var cookie = 0n
-var cookie2 = 0n
+var z = 0
+var zsec = 0
+var esclco = 10
+var resclco = 10
+var reste = 0
+var clickerpower = 1
+var dcco = 100
+var pescl = 0
+
 function Clicker(){
-    cookie++
-    cookie2++
-    document.getElementById('clickernb').innerHTML = cookie
+    z+=clickerpower
+    document.getElementById('clickernb').innerHTML = z
+}
+
+function gain(){
+    if(Math.floor(zsec) != zsec){
+        z+=Math.floor(zsec)
+        reste += zsec-Math.floor(zsec)
+        while(reste > 1){
+            z++
+            reste-=1
+        }
+    }
+    else {
+        z+=zsec
+    }
+    document.getElementById('clickernb').innerHTML = z
+}
+setInterval(gain, 1000)
+
+function esclb(){
+    if(z>=esclco){
+        z-=esclco
+        pescl++
+        zsec=(zsec*10+1)/10
+        esclco = Math.floor((resclco*12)/10)
+        resclco = (resclco*12)/10
+        document.getElementById('clickernbs').innerHTML = zsec
+        document.getElementById('esclco').innerHTML = esclco
+        document.getElementById('clickernb').innerHTML = z
+        document.getElementById('pescl').innerHTML = pescl
+    }
+}
+
+function dcb(){
+    if(z>=dcco){
+        z-=dcco
+        clickerpower+=1
+        document.getElementById("dc").style.display = "none"
+        document.getElementById("dcl").style.display = "none"
+    }
 }
