6e67ad2d62cb2fa4dc0f830a14142fcc26ed3b10
Zevier <87379749+Zevier02@users.noreply.github.com>
2024-10-29 19:12:40 +0100

    Add files via upload

--- scripts/bgcolor.js
+++ scripts/bgcolor.js
@@ -1,10 +1,10 @@
 var checkBox = document.getElementById("color");
 var colors = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)]
 document.body.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-document.getElementById('shopt').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
-document.getElementById('upgrades').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
-document.getElementById('buildings').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
-document.getElementById('clickergame').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6);`
+document.getElementById('shopt').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6); background-color: rgba(255, 0, 0, .3)`
+document.getElementById('upgrades').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6); background-color: rgba(255, 215, 0, .3)`
+document.getElementById('buildings').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6); background-color: rgba(128, 128, 128, .3)`
+document.getElementById('clickergame').style = `border-color: rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, .6); background-color: rgba(0, 0, 255, .3)`
 function a(){
     if(checkBox.checked){
         colors = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)]
