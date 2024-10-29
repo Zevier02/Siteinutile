0d3bace3372a81b085612f33a7553cd9c16c6793
Zevier <87379749+Zevier02@users.noreply.github.com>
2024-10-29 16:12:19 +0100

    Add files via upload

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -6,13 +6,20 @@ var reste = 0
 var clickerpower = 1
 var dcco = 100
 var pescl = 0
+var stagco = 100
+var rstagco = 100
+var pstag = 0
+var gescl = 0.1
+var gstag = 0.5
+var foua = false
+var fouco = 500
 
 function Clicker(){
     z+=clickerpower
     document.getElementById('clickernb').innerHTML = z
 }
 
-function gain(){
+function maj(){
     if(Math.floor(zsec/100) != zsec/100){
         z+=Math.floor(zsec/100)
         reste += zsec/100-Math.floor(zsec/100)
@@ -22,23 +29,41 @@ function gain(){
         }
     }
     else {
-        z+=zsec
+        z+=zsec/100
     }
     document.getElementById('clickernb').innerHTML = z
 }
-setInterval(gain, 10)
+setInterval(maj, 10)
 
 function esclb(){
     if(z>=esclco){
         z-=esclco
         pescl++
-        zsec=(zsec*10+1)/10
+        zsec=(zsec*10+gescl*10)/10
         esclco = Math.floor((resclco*12)/10)
         resclco = (resclco*12)/10
         document.getElementById('clickernbs').innerHTML = zsec
         document.getElementById('esclco').innerHTML = esclco
         document.getElementById('clickernb').innerHTML = z
         document.getElementById('pescl').innerHTML = pescl
+        if(pescl >= 10 && !foua){
+            document.getElementById("fou").style.display = "block"
+            document.getElementById("foul").style.display = "block"
+        }
+    }
+}
+
+function stagb(){
+    if(z>=stagco){
+        z-=stagco
+        pstag++
+        zsec=(zsec*10+gstag*10)/10
+        stagco = Math.floor((rstagco*12)/10)
+        rstagco = (rstagco*12)/10
+        document.getElementById('clickernbs').innerHTML = zsec
+        document.getElementById('stagco').innerHTML = stagco
+        document.getElementById('clickernb').innerHTML = z
+        document.getElementById('pstag').innerHTML = pstag
     }
 }
 
@@ -48,5 +73,17 @@ function dcb(){
         clickerpower+=1
         document.getElementById("dc").style.display = "none"
         document.getElementById("dcl").style.display = "none"
+    }
+}
+function foub(){
+    if(z>=fouco){
+        z-=fouco
+        gescl+=0.1
+        zsec = (gescl*pescl*100 + gstag*pstag*100)/100
+        document.getElementById("fou").style.display = "none"
+        document.getElementById("foul").style.display = "none"
+        document.getElementById("gescl").innerHTML = gescl
+        document.getElementById('clickernbs').innerHTML = zsec
+        foua = true
     }
 }
